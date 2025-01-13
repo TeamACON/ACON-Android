d333d3c073c0371bda804d5636bbd18ff95365a2
Thirfir <sangbbang31@gmail.com>
2025-01-13 23:29:33 +0900

    Add: Result 에러코드 확장함수

---
+++ data/src/main/kotlin/com/acon/data/error/ErrorUtils.kt
@@ -0,0 +1,20 @@
+package com.acon.data.error
+
+import kotlin.coroutines.cancellation.CancellationException
+
+internal inline fun <R> runCatchingWith(
+    vararg definedErrors: RootError,
+    block: () -> R
+): Result<R> {
+    return try {
+        Result.success(block())
+    } catch (e: RemoteError) {
+        definedErrors.find { definedError ->
+            e.errorCode == definedError.code
+        }?.let { Result.failure(it) } ?: Result.failure(e)
+    } catch (e: CancellationException) {
+        throw e
+    } catch (e: Throwable) {
+        Result.failure(e)
+    }
+}
