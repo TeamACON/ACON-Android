cb97d6bc8978f4b7f2a70b14479e75e8989fad57
seognmin <arcn1345@gmail.com>
2025-01-14 16:59:24 +0900

    Feat: AconFilledLargeButton, AconFilledMediumButton 추가

---
+++ core/designsystem/src/main/java/com/acon/core/designsystem/component/button/AconFilledMediumButton.kt
@@ -0,0 +1,61 @@
+package com.acon.core.designsystem.component.button
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.acon.core.designsystem.theme.AconTheme
+
+@Composable
+fun AconFilledMediumButton(
+    text: String,
+    textStyle: TextStyle,
+    enabledBackgroundColor: Color,
+    disabledBackgroundColor: Color,
+    textColor: Color = AconTheme.color.White,
+    isEnabled: Boolean = true,
+    cornerRadius: Dp = 4.dp,
+    modifier: Modifier = Modifier,
+    paddingHorizontal: Dp = 14.dp,
+    paddingVertical: Dp = 14.dp,
+    onClick: () -> Unit,
+) {
+    AconButton(
+        backGroundColor = if(isEnabled) enabledBackgroundColor else disabledBackgroundColor,
+        modifier = modifier,
+        cornerRadius = cornerRadius,
+        paddingHorizontal = paddingHorizontal,
+        paddingVertical = paddingVertical,
+        onClick = onClick,
+    ) {
+        Text(
+            text = text,
+            style = textStyle,
+            color = textColor
+        )
+    }
+}
+
+@Preview
+@Composable
+internal fun PreviewAconMediumButton() {
+    AconTheme {
+        AconFilledMediumButton(
+            text = "button",
+            textStyle = AconTheme.typography.head8_16_sb,
+            textColor = AconTheme.color.White,
+            enabledBackgroundColor = AconTheme.color.Main_org1,
+            disabledBackgroundColor =  AconTheme.color.Main_org1,
+            isEnabled = true,
+            cornerRadius = 4.dp,
+            modifier = Modifier,
+            paddingHorizontal = 16.dp,
+            paddingVertical = 14.dp,
+            onClick = {}
+        )
+    }
+}
