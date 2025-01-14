cc583de7f182f68709c0f1a8a4db4988b46be234
seognmin <arcn1345@gmail.com>
2025-01-15 02:10:37 +0900

    Feat: SignInScreenContent 추가

---
+++ feature/signin/src/main/java/com/acon/feature/signin/SignInScreenContent.kt
@@ -0,0 +1,16 @@
+package com.acon.feature.signin
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SignInScreenContent(
+
+) {
+    Column(
+        modifier = Modifier
+    ) {
+
+    }
+}
