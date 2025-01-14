b3e822b98c5f73185ee55edf127f44e6e0a87171
Thirfir <sangbbang31@gmail.com>
2025-01-14 14:47:06 +0900

    Add: SpotListRequest

---
+++ data/src/main/kotlin/com/acon/data/request/SpotListRequest.kt
@@ -0,0 +1,25 @@
+package com.acon.data.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SpotListRequest(
+    @SerialName("latitude") val latitude: Double,
+    @SerialName("longitude") val longitude: Double,
+    @SerialName("condition") val condition: ConditionRequest,
+    @SerialName("walkingTime") val walkingTime: Int,
+    @SerialName("priceRange") val priceRange: Int,
+)
+
+@Serializable
+data class ConditionRequest(
+    @SerialName("spotType") val spotType: String,
+    @SerialName("filterList") val filterList: List<FilterListRequest>,
+)
+
+@Serializable
+data class FilterListRequest(
+    @SerialName("category") val category: String,
+    @SerialName("optionList") val optionList: List<String>,
+)
