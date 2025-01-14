b55d018ca2119d5c09d9b89e6b86cd7723e3625f
Thirfir <sangbbang31@gmail.com>
2025-01-14 14:45:23 +0900

    Add: Option Types

---
+++ domain/src/main/java/com/acon/domain/type/OptionTypes.kt
@@ -0,0 +1,17 @@
+package com.acon.domain.type
+
+enum class RestaurantFeatureOptionType {
+    KOREAN, JAPANESE, CHINESE, WESTERN, SNACK, ASIAN, BAR, EXCLUDE_FRANCHISE
+}
+
+enum class CafeFeatureOptionType {
+    LARGE, GOOD_VIEW, DESSERT, TERRACE, EXCLUDE_FRANCHISE
+}
+
+enum class CompanionTypeOptionType {
+    FAMILY, DATE, FRIEND, ALONE, GROUP
+}
+
+enum class VisitPurposeOptionType {
+    MEETING, STUDY
+}
