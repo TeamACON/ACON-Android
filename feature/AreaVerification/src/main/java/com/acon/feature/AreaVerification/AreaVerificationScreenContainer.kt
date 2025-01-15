d3cf4e0ef6877be33fa1d459dc44547f5b10a8c1
0se0 <030ggong@gmail.com>
2025-01-15 12:43:41 +0900

    Feature: Create AreaVerificationScreenContainer

---
+++ feature/AreaVerification/src/main/java/com/acon/feature/AreaVerification/AreaVerificationScreenContainer.kt
@@ -0,0 +1,151 @@
+package com.acon.feature.AreaVerification
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.acon.core.designsystem.component.button.AconFilledLargeButton
+import com.acon.core.designsystem.theme.AconTheme
+import com.acon.feature.AreaVerification.component.AreaVerificationButton
+import android.provider.Settings
+import com.acon.core.designsystem.component.dialog.AconOneButtonDialog
+
+@Composable
+fun AreaVerificationScreenContainer(
+    onNewAreaClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: AreaVerificationViewModel = hiltViewModel(),
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    val context = LocalContext.current
+
+    val locationPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            onNewAreaClick()
+        } else {
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                viewModel.updateShowPermissionDialog(true)
+            }
+        }
+    }
+
+    AreaVerificationScreen(
+        uiState = uiState,
+        onNewLocationSelected = { viewModel.onNewLocationSelected() },
+        onDismissPermissionDialog = { viewModel.updateShowPermissionDialog(false) },
+        onPermissionSettingClick = {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.fromParts("package", context.packageName, null)
+            }
+            context.startActivity(intent)
+            viewModel.updateShowPermissionDialog(false)
+        },
+        onNextButtonClick = {
+            locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun AreaVerificationScreen(
+    uiState: AreaVerificationUiState,
+    onNewLocationSelected: () -> Unit,
+    onDismissPermissionDialog: () -> Unit,
+    onPermissionSettingClick: () -> Unit,
+    onNextButtonClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    if (uiState.showPermissionDialog) {
+        AconOneButtonDialog(
+            title = "위치 권한 필요",
+            content = "정확한 위치 확인이 필요합니다.\n설정에서 '정확한 위치' 권한을 허용해주세요.",
+            buttonContent = "설정으로 이동",
+            onDismissRequest = onDismissPermissionDialog,
+            onClickConfirm = onPermissionSettingClick,
+            isImageEnabled = false
+        )
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(AconTheme.color.Gray9)
+            .padding(horizontal = 24.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = "로컬 맛집 추천을 위해\n지역 인증이 필요해요.",
+                style = AconTheme.typography.head6_20_sb,
+                color = AconTheme.color.Gray1
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "지주 거는 동네로 지역 인증을 해보세요",
+                style = AconTheme.typography.body3_13_reg,
+                color = AconTheme.color.Gray3
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                AreaVerificationButton(
+                    selected = uiState.isNewLocationSelected,
+                    onClick = onNewLocationSelected,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+
+        AconFilledLargeButton(
+            text = "다음",
+            textStyle = AconTheme.typography.head8_16_sb,
+            enabledBackgroundColor = AconTheme.color.Gray5,
+            disabledBackgroundColor = AconTheme.color.Gray8,
+            enabledTextColor = AconTheme.color.White,
+            disabledTextColor = AconTheme.color.Gray6,
+            onClick = onNextButtonClick,
+            isEnabled = uiState.isNewLocationSelected,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 24.dp)
+        )
+    }
+}
