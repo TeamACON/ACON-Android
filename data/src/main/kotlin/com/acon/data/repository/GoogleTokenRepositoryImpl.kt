909e54b2d3105547eefc1cac2ed396a9dc1146d0
seognmin <arcn1345@gmail.com>
2025-01-15 23:03:36 +0900

    Mod: GoogleTokenRepositoryImpl 로직 수정 (바로 소셜 로그인 API 호출)

--- data/src/main/kotlin/com/acon/data/repository/GoogleTokenRepositoryImpl.kt
+++ data/src/main/kotlin/com/acon/data/repository/GoogleTokenRepositoryImpl.kt
@@ -1,13 +1,21 @@
 package com.acon.data.repository
 
 import com.acon.data.datasource.remote.TokenRemoteDataSource
+import com.acon.domain.repository.AuthRepository
 import com.acon.domain.repository.GoogleTokenRepository
+import com.acon.domain.type.SocialType
 import javax.inject.Inject
 
 class GoogleTokenRepositoryImpl @Inject constructor(
-    private val googleSignInDataSource: TokenRemoteDataSource
+    private val tokenRemoteDataSource: TokenRemoteDataSource,
+    private val authRepository: AuthRepository
 ): GoogleTokenRepository {
-    override suspend fun signIn(): Result<Unit> =
-        googleSignInDataSource.signIn()
-
+    override suspend fun signIn(): Result<Unit> {
+        return tokenRemoteDataSource.signIn().mapCatching { idToken ->
+            authRepository.postLogin(
+                socialType = SocialType.GOOGLE,
+                idToken = idToken
+            )
+        }.getOrThrow()
+    }
 }
