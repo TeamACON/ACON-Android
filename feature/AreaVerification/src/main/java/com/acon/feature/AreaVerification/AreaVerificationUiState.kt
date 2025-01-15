aa9c5e9d043bf0f2aaf143db635cb61d98ebc94b
0se0 <030ggong@gmail.com>
2025-01-15 14:41:33 +0900

    Update: AreaVerificationUiState

--- feature/AreaVerification/src/main/java/com/acon/feature/AreaVerification/AreaVerificationUiState.kt
+++ feature/AreaVerification/src/main/java/com/acon/feature/AreaVerification/AreaVerificationUiState.kt
@@ -4,5 +4,8 @@ data class AreaVerificationUiState(
     val isNewLocationSelected: Boolean = false,
     val isButtonEnabled: Boolean = true,
     val isLocationPermissionGranted: Boolean = false,
-    val showPermissionDialog: Boolean = false
+    val showPermissionDialog: Boolean = false,
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val isLocationObtained: Boolean = false
 )
