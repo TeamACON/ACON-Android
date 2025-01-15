afde51429c6fdae6e750a11941d075a6753b0225
tunaunnie <ygw021227@gmail.com>
2025-01-15 10:46:49 +0900

    Add: base 단에 baseContainerHost 구현 및 관련 dependencies 추가

--- core/base/build.gradle.kts
+++ core/base/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -30,6 +31,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
@@ -40,4 +44,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.orbit.viewmodel)
+    implementation(libs.orbit.compose)
 }
