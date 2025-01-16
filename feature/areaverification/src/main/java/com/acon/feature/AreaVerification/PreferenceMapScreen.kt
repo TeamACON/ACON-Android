bbce5aaea148cda8bc9b9a74349422fe1c170d67
0se0 <030ggong@gmail.com>
2025-01-16 10:50:47 +0900

    Feature: connection bottomsheet

--- feature/areaverification/src/main/java/com/acon/feature/AreaVerification/PreferenceMapScreen.kt
+++ feature/areaverification/src/main/java/com/acon/feature/AreaVerification/PreferenceMapScreen.kt
@@ -2,20 +2,52 @@ package com.acon.feature.areaverification
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.acon.core.designsystem.component.button.AconFilledLargeButton
 import com.acon.core.designsystem.theme.AconTheme
+import com.acon.feature.areaverification.component.DottoriSelectionBottomSheet
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PreferenceMapScreen(
     onConfirmClick: () -> Unit,
+    onNavigateToNext: () -> Unit,
     latitude: Double,
     longitude: Double,
     modifier: Modifier = Modifier
 ) {
+    var showBottomSheet by remember { mutableStateOf(false) }
+
+    if (showBottomSheet) {
+        val sheetState = rememberModalBottomSheetState(
+            skipPartiallyExpanded = true,
+        )
+
+        ModalBottomSheet(
+            onDismissRequest = { showBottomSheet = false },
+            containerColor = Color.Transparent,
+            dragHandle = null,
+            sheetState = sheetState,
+        ) {
+            DottoriSelectionBottomSheet(
+                onDismiss = { showBottomSheet = false },
+                onNavigateToNext = onNavigateToNext
+            )
+        }
+    }
+
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -47,7 +79,7 @@ fun PreferenceMapScreen(
             enabledBackgroundColor = AconTheme.color.Gray5,
             disabledBackgroundColor = AconTheme.color.Gray8,
             enabledTextColor = AconTheme.color.White,
-            onClick = onConfirmClick,
+            onClick = { showBottomSheet = true },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 24.dp, vertical = 16.dp)
