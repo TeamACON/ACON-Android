b08a3684c880570313b201a2e7a6c87c1a9e0e02
seognmin <arcn1345@gmail.com>
2025-01-16 15:19:19 +0900

    Feat: SignInScreenContent에서 SignInScreen 생성

--- feature/signin/src/main/java/com/acon/feature/signin/screen/SignInScreenContent.kt
+++ feature/signin/src/main/java/com/acon/feature/signin/screen/SignInScreenContent.kt
@@ -2,15 +2,21 @@ package com.acon.feature.signin.screen
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.acon.domain.repository.GoogleTokenRepository
 
 @Composable
 fun SignInScreenContent(
-    modifier: Modifier = Modifier
-) {
+    modifier: Modifier = Modifier,
+    googleSignInRepository: GoogleTokenRepository,
+    viewModel: SignInViewModel = hiltViewModel()
+    ) {
     SignInScreen(
         navigateToSpotLisView = {},
         onClickTermsOfUse = {},
         onClickPrivacyPolicy = {},
-        onClickLoginGoogle = {}
+        onClickLoginGoogle = {
+            viewModel.googleLogin(googleSignInRepository)
+        }
     )
 }
