9913f36048d5b8a141f1cc7702c6fa3f29745b57
0se0 <030ggong@gmail.com>
2025-01-17 11:48:05 +0900

    Update: dotori count state

--- feature/upload/src/main/java/com/acon/feature/upload/UploadState.kt
+++ feature/upload/src/main/java/com/acon/feature/upload/UploadState.kt
@@ -4,5 +4,6 @@ data class UploadState(
     val selectedCount: Int = 0,
     val maxCount: Int = 5,
     val ownedDotoriCount: Int = 25, //서버에서 받아 오는 도토리 수
-    val isButtonEnabled: Boolean = false
+    val isButtonEnabled: Boolean = false,
+    val animatingIndex: Int? = null
 )
