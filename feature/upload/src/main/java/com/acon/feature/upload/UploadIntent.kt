d7aa2ee602ced19a79397507a1cb00017aacc439
0se0 <030ggong@gmail.com>
2025-01-19 19:34:33 +0900

    Feature: Update UploadIntent

--- feature/upload/src/main/java/com/acon/feature/upload/UploadIntent.kt
+++ feature/upload/src/main/java/com/acon/feature/upload/UploadIntent.kt
@@ -1,8 +1,10 @@
 package com.acon.feature.upload
 
 sealed interface UploadIntent {
+    data class SelectLocation(val location: LocationItem) : UploadIntent
     data class SelectDotori(val index: Int) : UploadIntent
     data object DeselectDotori : UploadIntent
     data object NavigateBack : UploadIntent
+    data object OnNextStep : UploadIntent
     data object UploadDotori : UploadIntent
 }
