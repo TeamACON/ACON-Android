df124af94317504839878cd386479054d2fe599c
tunaunnie <ygw021227@gmail.com>
2025-01-21 18:33:45 +0900

    Add: 각 type에 id 추가

--- feature/onboarding/src/main/java/com/acon/feature/onboarding/type/PlaceItems.kt
+++ feature/onboarding/src/main/java/com/acon/feature/onboarding/type/PlaceItems.kt
@@ -6,16 +6,18 @@ import com.acon.feature.onboarding.R
 enum class PlaceItems(
     @DrawableRes override val imageResId: Int,
     override val cardName: String,
+    override val id: String
 ) : CardItem {
 
-    RESTAURANT(R.drawable.place_img_3, "음식점"),
-    CAFE(R.drawable.place_img_4, "카페"),
+    RESTAURANT(R.drawable.place_img_3, cardName = "음식점", id = "RESTAURANT"),
+    CAFE(R.drawable.place_img_4, cardName = "카페", id = "CAFE"),
 }
 
 enum class MoodItems(
     @DrawableRes override val imageResId: Int,
     override val cardName: String,
+    override val id: String
 ) : CardItem {
-    OLD(R.drawable.place_img_1, "빈티지"),
-    MODERN(R.drawable.place_img_2, "모던"),
+    OLD(R.drawable.place_img_1, cardName = "빈티지", id = "TRADITIONAL"),
+    MODERN(R.drawable.place_img_2, cardName = "모던", id = "MODERN"),
 }
--- feature/onboarding/src/main/java/com/acon/feature/onboarding/type/PreferPlaceItems.kt
+++ feature/onboarding/src/main/java/com/acon/feature/onboarding/type/PreferPlaceItems.kt
@@ -6,9 +6,10 @@ import com.acon.feature.onboarding.R
 enum class PreferPlaceItems(
     @DrawableRes override val imageResId: Int,
     override val cardName: String,
+    override val id: String
 ) : CardItem {
-    MOOD(R.drawable.placetype_img_1, "분위기가 감각적인"),
-    NEWFOOD(R.drawable.placetype_img_2, "새로운 음식의 경험"),
-    CHEAP(R.drawable.placetype_img_3, "합리적인 가격과 양"),
-    SPECIAL(R.drawable.placetype_img_4, "특별한 날, 고급스러운"),
+    MOOD(R.drawable.placetype_img_1, cardName = "분위기가 감각적인", id = "SENSE"),
+    NEWFOOD(R.drawable.placetype_img_2, cardName = "새로운 음식의 경험", id = "NEW_FOOD"),
+    CHEAP(R.drawable.placetype_img_3, cardName = "합리적인 가격과 양", id = "REASONABLE"),
+    SPECIAL(R.drawable.placetype_img_4, cardName = "특별한 날, 고급스러운", id = "LUXURY"),
 }
