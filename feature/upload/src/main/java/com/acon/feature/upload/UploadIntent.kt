fdedf8a0bf276143021dc42e834869bf70085e04
0se0 <030ggong@gmail.com>
2025-01-22 03:23:16 +0900

    Feature: Update UploadIntent

--- feature/upload/src/main/java/com/acon/feature/upload/UploadIntent.kt
+++ feature/upload/src/main/java/com/acon/feature/upload/UploadIntent.kt
@@ -1,10 +1,16 @@
 package com.acon.feature.upload
 
+import com.acon.domain.model.upload.SpotListItem
+
 sealed interface UploadIntent {
-    data class SelectLocation(val location: LocationItem) : UploadIntent
+    data class SelectLocation(val location: SpotListItem) : UploadIntent
     data class SelectDotori(val index: Int) : UploadIntent
     data class DeselectDotori(val index: Int) : UploadIntent
     data object NavigateBack : UploadIntent
     data object OnNextStep : UploadIntent
     data object UploadDotori : UploadIntent
+    data class LoadSuggestions(
+        val latitude: Double,
+        val longitude: Double
+    ) : UploadIntent
 }
