8eb84155b76b91ca9696d76a2a119014f6d86311
0se0 <030ggong@gmail.com>
2025-01-22 03:23:25 +0900

    Feature: Update UploadState

--- feature/upload/src/main/java/com/acon/feature/upload/UploadState.kt
+++ feature/upload/src/main/java/com/acon/feature/upload/UploadState.kt
@@ -1,12 +1,19 @@
 package com.acon.feature.upload
 
+import com.acon.domain.model.upload.SpotListItem
+import com.acon.domain.model.upload.Suggestion
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+
 data class UploadState(
-    val selectedLocation: LocationItem? = null,
-    val currentStep: UploadStep = UploadStep.Upload_Search,
+    val selectedLocation: SpotListItem? = null,
+    val currentStep: UploadStep = UploadStep.UPLOAD_SEARCH,
     val selectedCount: Int = 0,
     val maxCount: Int = 5,
-    val ownedDotoriCount: Int = 25, //from server
     val isButtonEnabled: Boolean = false,
     val animatingIndex: Int? = null,
-    val isLocationSearchVisible: Boolean = false
+    val isLocationSearchVisible: Boolean = false,
+    val ownedDotoriCount: Int = 0,
+    val searchResults: PersistentList<SpotListItem> = persistentListOf(),
+    val suggestions: PersistentList<Suggestion> = persistentListOf(),
 )
