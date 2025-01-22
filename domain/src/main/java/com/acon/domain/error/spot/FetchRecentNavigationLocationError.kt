8bdfbbabf6b9688c44f76e148534cb3206bb874d
seognmin <arcn1345@gmail.com>
2025-01-23 05:36:59 +0900

    Feat: FetchRecentNavigationLocationError 추가

---
+++ domain/src/main/java/com/acon/domain/error/spot/FetchRecentNavigationLocationError.kt
@@ -0,0 +1,19 @@
+package com.acon.domain.error.spot
+
+import com.acon.domain.error.ErrorFactory
+import com.acon.domain.error.RootError
+
+sealed class FetchRecentNavigationLocationError : RootError() {
+
+    class SpaceNotFoundError : FetchRecentNavigationLocationError() {
+        override val code: Int = 40403
+    }
+
+    companion object : ErrorFactory {
+        override fun createErrorInstances(): Array<RootError> {
+            return arrayOf(
+                SpaceNotFoundError(),
+            )
+        }
+    }
+}
