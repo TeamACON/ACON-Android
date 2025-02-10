cb46aa7f8639b62b5a762c54c4dc207f5f113391
seognmin <arcn1345@gmail.com>
2025-02-10 20:58:54 +0900

    Feat: DeleteAccountScreen, DeleteAccountScreenContainer 추가

---
+++ feature/settings/src/main/java/com/acon/feature/withdraw/screen/DeleteAccountScreen.kt
@@ -0,0 +1,22 @@
+package com.acon.feature.withdraw.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.acon.core.designsystem.theme.AconTheme
+
+@Composable
+fun DeleteAccountScreen(
+
+) {
+
+}
+
+@Preview
+@Composable
+fun DeleteAccountScreenPreview() {
+    AconTheme {
+        DeleteAccountScreen(
+
+        )
+    }
+}
---
+++ feature/settings/src/main/java/com/acon/feature/withdraw/screen/DeleteAccountScreenContainer.kt
@@ -0,0 +1,10 @@
+package com.acon.feature.withdraw.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DeleteAccountScreenContainer(
+
+) {
+
+}
