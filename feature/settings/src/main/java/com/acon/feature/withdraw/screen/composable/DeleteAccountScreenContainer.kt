69c0dee36e991849cf4703d1001ae59f5a0999cf
seognmin <arcn1345@gmail.com>
2025-02-12 04:10:43 +0900

    Feat: DeleteAccountScreen, Container, ViewModel 추가

--- feature/settings/src/main/java/com/acon/feature/withdraw/screen/composable/DeleteAccountScreenContainer.kt
+++ feature/settings/src/main/java/com/acon/feature/withdraw/screen/composable/DeleteAccountScreenContainer.kt
@@ -1,10 +1,36 @@
 package com.acon.feature.withdraw.screen.composable
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.acon.feature.withdraw.screen.DeleteAccountSideEffect
+import com.acon.feature.withdraw.screen.DeleteAccountViewModel
+import org.orbitmvi.orbit.compose.collectAsState
+import org.orbitmvi.orbit.compose.collectSideEffect
 
 @Composable
 fun DeleteAccountScreenContainer(
+    modifier: Modifier = Modifier,
+    navigateToSettings: () -> Unit = {},
+    navigateToSignIn: () -> Unit = {},
+    viewModel: DeleteAccountViewModel = hiltViewModel()
 
 ) {
+    val state by viewModel.collectAsState()
 
+    DeleteAccountScreen(
+        state = state,
+        modifier = modifier,
+        navigateBack = viewModel::navigateBack,
+        onDeleteAccount = viewModel::onDeleteAccount,
+        onBottomSheetShowStateChange = viewModel::onDeleteAccountBottomSheetShowStateChange
+    )
+
+    viewModel.collectSideEffect {
+        when(it) {
+            is DeleteAccountSideEffect.NavigateToSettings -> navigateToSettings()
+            is DeleteAccountSideEffect.NavigateToSignIn -> navigateToSignIn()
+        }
+    }
 }
