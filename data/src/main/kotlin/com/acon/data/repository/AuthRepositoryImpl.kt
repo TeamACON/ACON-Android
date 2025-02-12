4f002f1132a155b76ef1516dd47d1505a2e95815
seognmin <arcn1345@gmail.com>
2025-02-13 05:13:39 +0900

    Feat: 앱 전역에서 접근가능한 로그인/비로그인 상태 업데이트/조회 로직 추가

--- data/src/main/kotlin/com/acon/data/repository/AuthRepositoryImpl.kt
+++ data/src/main/kotlin/com/acon/data/repository/AuthRepositoryImpl.kt
@@ -7,12 +7,19 @@ import com.acon.data.error.runCatchingWith
 import com.acon.domain.error.auth.PostLoginError
 import com.acon.domain.repository.AuthRepository
 import com.acon.domain.type.SocialType
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val authRemoteDataSource: AuthRemoteDataSource,
     private val tokenLocalDataSource: TokenLocalDataSource,
 ): AuthRepository {
+
+    private val _isLogin = MutableStateFlow(false)
+    private val isLogin: StateFlow<Boolean> = _isLogin.asStateFlow()
+
     override suspend fun postLogin(
         socialType: SocialType,
         idToken: String
@@ -26,7 +33,13 @@ class AuthRepositoryImpl @Inject constructor(
             )
             googleLoginResponse.accessToken?.let { tokenLocalDataSource.saveAccessToken(it) }
             googleLoginResponse.refreshToken?.let { tokenLocalDataSource.saveRefreshToken(it) }
+
+            _isLogin.value = true
         }
+    }
+
+    override fun getLoginState(): StateFlow<Boolean> {
+        return isLogin
     }
 
 }
--- domain/src/main/java/com/acon/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/acon/domain/repository/AuthRepository.kt
@@ -1,7 +1,9 @@
 package com.acon.domain.repository
 
 import com.acon.domain.type.SocialType
+import kotlinx.coroutines.flow.StateFlow
 
 interface AuthRepository {
+    fun getLoginState(): StateFlow<Boolean>
     suspend fun postLogin(socialType: SocialType, idToken: String): Result<Unit>
 }
